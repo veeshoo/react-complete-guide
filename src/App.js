d463383ae8ff399121017379e954797fa3a1fe5e
veeshoo@gmail.com <veeshoo@gmail.com>
2018-07-28 23:18:03 +0530

    JS way of handling conditional statement

--- src/App.js
+++ src/App.js
@@ -47,26 +47,29 @@ class App extends Component {
       cursor: 'pointer'   
     }
 
+    let persons = null;
+    if(this.state.showPersons){
+      persons = 
+        <div>
+          <Person name={this.state.persons[0].name} 
+                  age={this.state.persons[0].age}
+                  changed={this.nameChangeHandler}>My Hobbies: Reading about Space</Person>
+          <Person name={this.state.persons[1].name} 
+                  age={this.state.persons[1].age}
+                  click={this.switchNameHandler.bind(this, "Baba")}
+                  changed={this.nameChangeHandler}/>
+          <Person name={this.state.persons[2].name} 
+                  age={this.state.persons[2].age}>I don't have any hobbies</Person>
+        </div>
+      ;
+    }
     return (
       <div className="App">
         <h1>Hi, I am a React App!</h1>
         <p>This is really working.</p>
         <button style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {
-          this.state.showPersons?
-          <div>
-            <Person name={this.state.persons[0].name} 
-                    age={this.state.persons[0].age}
-                    changed={this.nameChangeHandler}>My Hobbies: Reading about Space</Person>
-            <Person name={this.state.persons[1].name} 
-                    age={this.state.persons[1].age}
-                    click={this.switchNameHandler.bind(this, "Baba")}
-                    changed={this.nameChangeHandler}/>
-            <Person name={this.state.persons[2].name} 
-                    age={this.state.persons[2].age}>I don't have any hobbies</Person>
-          </div> : null
-        }
+        {persons}
       </div>
     );
   }
